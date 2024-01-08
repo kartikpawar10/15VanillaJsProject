ce379997d47fab1be986447565ef676c8d0037d3
kartikpawar10 <kartikpawarofficialon@gmail.com>
2024-01-08 16:49:50 +0530

    change in hamburger motion

--- pro4/app.js
+++ pro4/app.js
@@ -1,15 +1,10 @@
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector(".links");
+const btn = document.getElementById("btn")
 
 navToggle.addEventListener('click',() =>{
-    // console.log(links.classList.contains("random"));
-    // console.log(links.classList.contains("links"));
-
-    // if(links.classList.contains('show-links')){
-    //     links.classList.remove('show-links')
-    // }
-    // else{
-    //     links.classList.add('show-links')
-    // }
     links.classList.toggle("show-links");
+})
+btn.addEventListener('click',()=>{
+    btn.classList.toggle("round")
 })
